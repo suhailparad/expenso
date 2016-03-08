a35fb4823f91bae2440b66a23148964ec809ec97
suhail <pk.suhail47@gmail.com>
2016-03-08 20:10:11 +0530

    Committing again

---
+++ resources/views/add.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Add Expenso :: Expense Management System</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="<?php echo asset('css/site.css')?>" rel="stylesheet">
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
